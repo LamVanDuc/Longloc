88e669659a2add63011fdd55e9478485446c9407
LamVanDuc <88825052+LamVanDuc@users.noreply.github.com>
2022-08-23 12:35:55 +0700

    duc

---
+++ src/main/java/com/example/projectsem2/controller/view/nomal/ApplicationController.java
@@ -0,0 +1,45 @@
+package com.example.projectsem2.controller.view.nomal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ApplicationController {
+
+    @RequestMapping(value = {"","/","/home","/index"},method = RequestMethod.GET)
+    public String getIndex(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "Login";
+    }
+
+    @GetMapping("/register")
+    public String getRegister(){
+        return "Register";
+    }
+
+    @GetMapping("/contact")
+    public String getContact(){
+        return "contact";
+    }
+
+    @GetMapping("/shop")
+    public String getShop(){
+        return "shop";
+    }
+
+    @GetMapping("/layout")
+    public String getLayout(){
+        return "layout";
+    }
+
+    @GetMapping("/blog")
+    public String getBlog(){
+        return "blog";
+    }
+}
