4795ea15f93c7d19d306a81665c7794e3b86bbeb
LamVanDuc <88825052+LamVanDuc@users.noreply.github.com>
2022-08-23 12:14:04 +0700

    Update DonHangController.java

--- src/main/java/com/example/projectsem2/controller/api/DonHangController.java
+++ src/main/java/com/example/projectsem2/controller/api/DonHangController.java
@@ -3,10 +3,22 @@ package com.example.projectsem2.controller.api;
 import com.example.projectsem2.Service.DonHangService;
 import com.example.projectsem2.dto.dtoChiTietDonHang;
 import com.example.projectsem2.dto.responseObject;
+<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+=======
+
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+>>>>>>> main
 
 @RestController
 @RequestMapping("/api/v1/donhang")
@@ -14,6 +26,7 @@ public class DonHangController {
     @Autowired
     DonHangService donHangService;
 
+<<<<<<< HEAD
 //    @GetMapping("/q")
 //    public ResponseEntity<responseObject> getById(@RequestParam("id")String id){
 //        try{
@@ -25,4 +38,22 @@ public class DonHangController {
 //        }
 //
 //    }
+=======
+    @GetMapping("/q")
+    public ResponseEntity<responseObject> findByIdDonhang(@RequestParam(value = "id") String id){
+        try{
+            dtoChiTietDonHang chiTietDonHang =  donHangService.getByIdDonHang(id);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new responseObject("ok" ,"query thành công !",chiTietDonHang));
+
+        }catch (NotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new responseObject("false" , "Đơn hàng không tồn tại!",""));
+
+        }catch (Exception exx){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new responseObject("false" , "Đã sảy ra lỗi : "+exx.getMessage(),""));
+        }
+    }
+>>>>>>> main
 }
