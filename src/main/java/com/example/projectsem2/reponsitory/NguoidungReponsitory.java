9a94d34767a8c07752c60ac444a4f4eb62a68a88
LamVanDuc <88825052+LamVanDuc@users.noreply.github.com>
2022-09-02 23:26:29 +0700

    duc 02/09/2022

--- src/main/java/com/example/projectsem2/reponsitory/NguoidungReponsitory.java
+++ src/main/java/com/example/projectsem2/reponsitory/NguoidungReponsitory.java
@@ -2,6 +2,7 @@ package com.example.projectsem2.reponsitory;
 
 import com.example.projectsem2.entity.tblNguoidung;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
